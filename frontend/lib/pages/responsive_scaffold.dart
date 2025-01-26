cf7d3ad4a5b0de92136b1e98c8cb8b05bc89e1c1
Tom W. Yedwab <sincerelyyourstom@gmail.com>
2025-01-26 11:28:36 -0800

    Responsive nav moves top-level nav to left

--- frontend/lib/pages/responsive_scaffold.dart
+++ frontend/lib/pages/responsive_scaffold.dart
@@ -11,38 +11,36 @@ class ResponsiveScaffold extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        final bottomNavigationBar = NavigationBar(
-          indicatorColor: const Color(0xff7faad0),
-          onDestinationSelected: (int index) {
-            if (index == 0) {
-              context.go('/');
-            } else if (index == 1) {
-              context.go('/templates');
-            } else if (index == 2) {
-              context.go('/archived');
-            }
-          },
-          selectedIndex: selectedIndex,
-          destinations: const <NavigationDestination>[
-            NavigationDestination(
-              icon: Icon(Icons.list),
-              label: 'Lists',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.copy_all),
-              label: 'Templates',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.archive),
-              label: 'Archived',
-            ),
-          ],
-        );
-        
         if (constraints.maxWidth < 860) {
           return Scaffold(
             body: children[children.length - 1],
-            bottomNavigationBar: bottomNavigationBar,
+            bottomNavigationBar: NavigationBar(
+              indicatorColor: const Color(0xff7faad0),
+              onDestinationSelected: (int index) {
+                if (index == 0) {
+                  context.go('/');
+                } else if (index == 1) {
+                  context.go('/templates');
+                } else if (index == 2) {
+                  context.go('/archived');
+                }
+              },
+              selectedIndex: selectedIndex,
+              destinations: const <NavigationDestination>[
+                NavigationDestination(
+                  icon: Icon(Icons.list),
+                  label: 'Lists',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.copy_all),
+                  label: 'Templates',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.archive),
+                  label: 'Archived',
+                ),
+              ],
+            ),
           );
         }
         
@@ -50,12 +48,43 @@ class ResponsiveScaffold extends StatelessWidget {
         return Scaffold(
           body: Row(
             children: [
+              NavigationRail(
+                indicatorColor: const Color(0xff7faad0),
+                labelType: NavigationRailLabelType.all,
+                groupAlignment: 0.0,
+                onDestinationSelected: (int index) {
+                  if (index == 0) {
+                    context.go('/');
+                  } else if (index == 1) {
+                    context.go('/templates');
+                  } else if (index == 2) {
+                    context.go('/archived');
+                  }
+                },
+                selectedIndex: selectedIndex,
+                destinations: const <NavigationRailDestination>[
+                  NavigationRailDestination(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    icon: Icon(Icons.list),
+                    label: Text('Lists'),
+                  ),
+                  NavigationRailDestination(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    icon: Icon(Icons.copy_all),
+                    label: Text('Templates'),
+                  ),
+                  NavigationRailDestination(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    icon: Icon(Icons.archive),
+                    label: Text('Archived'),
+                  ),
+                ],
+              ),
               // First column
               SizedBox(
                 width: 250,
                 child: Scaffold(
                   body: children[0],
-                  bottomNavigationBar: bottomNavigationBar,
                 ),
               ),
               if (children.length > 1) ...[
